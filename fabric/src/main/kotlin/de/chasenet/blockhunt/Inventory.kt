134dd53d5e7b5794ca8ab1e2b0c7f67f698d57a8
Chase22 <chase@mailbox.org>
2024-01-17 14:30:06 +0100

    Fix starting a blockhunt cleared the starter kit

--- fabric/src/main/kotlin/de/chasenet/blockhunt/Inventory.kt
+++ fabric/src/main/kotlin/de/chasenet/blockhunt/Inventory.kt
@@ -2,8 +2,9 @@ package de.chasenet.blockhunt
 
 import de.chasenet.blockhunt.config.BlockHuntConfig
 import net.minecraft.entity.player.PlayerInventory
+import net.minecraft.item.ItemStack
 
 fun clearAndAddKit(inventory: PlayerInventory) {
     inventory.clear()
-    BlockHuntConfig.instance.starterKit.forEach(inventory::offerOrDrop)
+    BlockHuntConfig.instance.starterKit.map(ItemStack::copy).forEach(inventory::offerOrDrop)
 }
